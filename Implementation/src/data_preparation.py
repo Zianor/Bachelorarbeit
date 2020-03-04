7db9cf1c06e0ea2e8092801599799184b71af366
Zianor <zianor@zianor.de>
2020-03-04 17:32:21 +0100

    Implemented structure of the classes BcgData and DataSeries

--- Implementation/src/data_preparation.py
+++ Implementation/src/data_preparation.py
@@ -3,7 +3,42 @@ from src.utils import get_project_root
 import matplotlib.pyplot as plt
 import os
 
-mat_dict = loadmat(os.path.join(get_project_root(), 'data/ML_data_patient_1.mat'))
-print(mat_dict.keys())
-plt.plot(mat_dict['BBI_BCG']-mat_dict['BBI_ECG'])
-plt.show()
+
+class DataSeries:
+
+    def __init__(self, raw_data, sqi, bbi_bcg, bbi_ecg, indices, samplerate):
+        self.raw_data = raw_data
+        self.sqi = sqi
+        self.bbi_bcg = bbi_bcg
+        self.bbi_ecg = bbi_ecg
+        self.indices = indices
+        self.samplerate = samplerate
+
+    def calculate_bbi_error(self):
+        return abs(self.bbi_bcg - self.bbi_ecg)
+
+
+class BcgData:
+    samplerate = 100
+
+    def __init__(self):
+        paths = [path for path in os.listdir(os.path.join(get_project_root(), 'data/')) if path.lower().endswith(".mat")]
+        paths = [os.path.join(os.path.join(get_project_root(), 'data/'), path) for path in paths]
+        self.data_series = []
+        for path in paths:
+            mat_dict = loadmat(path)
+            self.data_series.append(
+                DataSeries(
+                    mat_dict['BCG_raw_data'],
+                    mat_dict['q_BCG'],
+                    mat_dict['BBI_BCG'],
+                    mat_dict['BBI_ECG'],
+                    mat_dict['indx'],
+                    self.samplerate)
+            )
+
+
+data = BcgData()
+for series in data.data_series:
+    plt.plot(series.calculate_bbi_error())
+    plt.show()
