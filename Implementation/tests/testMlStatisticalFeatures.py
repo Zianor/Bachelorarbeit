16447b959d43cc9096cc4ea8d55ef9420389be92
Zianor <zianor@zianor.de>
2020-03-24 15:05:49 +0100

    Adapted test to changes

--- Implementation/tests/testMlStatisticalFeatures.py
+++ Implementation/tests/testMlStatisticalFeatures.py
@@ -3,8 +3,11 @@ from ml_statistical_features import DataSet
 
 
 class MyTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.data = DataSet()
+
     def test_seconds_to_frames(self):
-        self.assertEqual(DataSet._seconds_to_frames(1.003, 100), int(1.003*100))
+        self.assertEqual(self.data._seconds_to_frames(1.003, 100), int(1.003*100))
 
 
 if __name__ == '__main__':
