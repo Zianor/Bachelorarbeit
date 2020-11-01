6adced59adbd4fbb33698a3f05ed6da2d4ca115a
Cay Rahn <cay.rahn@rwth-aachen.de>
2020-11-01 15:58:59 +0100

    feat: added data folder to reproduction script

--- Implementation/src/reproduce_results.py
+++ Implementation/src/reproduce_results.py
@@ -1,11 +1,13 @@
 from data_statistical_features import *
 
 
-def reproduce_own_segments(segment_lengths=[5, 10, 20, 30], overlap_amounts=[0.8, 0.9, 0.9, 0.9]):
+def reproduce_own_segments(segment_lengths=[5, 10, 20, 30], overlap_amounts=[0.8, 0.9, 0.9, 0.9],
+                           data_folder='data_patients'):
     assert len(segment_lengths)==len(overlap_amounts)
     for segment_length, overlap_amount in zip(segment_lengths, overlap_amounts):
-        DataSetOwn(segment_length=segment_length, overlap_amount=overlap_amount)
+        DataSetOwn(segment_length=segment_length, overlap_amount=overlap_amount, data_folder=data_folder)
 
 
 if __name__ == "__main__":
-    reproduce_own_segments(segment_lengths=[20, 30], overlap_amounts=[0.9, 0.9])
+    reproduce_own_segments(segment_lengths=[10], overlap_amounts=[0.9], data_folder='data_healthy')
+    # reproduce_own_segments(segment_lengths=[20, 30], overlap_amounts=[0.9, 0.9])
